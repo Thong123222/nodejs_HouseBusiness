46366852cacbc854268792d0a5b41cda6d0707b3
ducthong <ngduchong01@gmail.com>
2024-09-06 09:31:23 +0700

    Mô tả ngắn gọn về thay đổi

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const hbr = require('express-handlebars');
+const path = require('path');
+
+//Static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbr', hbr.engine({
+  extname: '.hbr',
+}));
+app.set('view engine', 'hbr');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
