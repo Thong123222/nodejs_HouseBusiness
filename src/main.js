a7efa550b4176fa1b46aaabb409bcf13d0ab17dd
ducthong <ngduchong01@gmail.com>
2024-09-12 15:10:06 +0700

    thay doi toan bo

--- src/main.js
+++ src/main.js
@@ -25,6 +25,10 @@ app.get('/news', (req, res) => {
   res.render('news');
 });
 
+app.get('/login', (req, res) => {
+  res.render('login', { layout: 'login'});
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
